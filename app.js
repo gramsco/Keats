035e90f5b085e43b0eae4e1ebc3ce73d5d887429
gramsco <antoningauth@gmail.com>
2019-08-31 14:54:25 +0200

    tryouts

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let app = express();
 const hbs = require("hbs")
 const path = require("path")
 const url = 'http://poetrydb.org/author/keats';
+const PORT = process.env.PORT || 7000;
 
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -13,7 +14,7 @@ app.set("view engine", "hbs")
 hbs.registerPartials(path.join(__dirname,"views/partials"))
 
 app.get("/", (req, res) => {
-
+    // res.send("all good");
     axios.get(url)
         .then((resp) => {
             let random = Math.floor(Math.random()*resp.data.length)
@@ -21,23 +22,10 @@ app.get("/", (req, res) => {
             res.render("index", poem)
         })
     .catch((err) => console.log(err))
-    
 })
 
-app.listen(3000, () => console.log("works at http://localhost:3000"));
+app.listen(PORT, () => console.log(`works at http://localhost:${PORT}`));
 
-
-// function getData(url) {
-//     return axios.get(url)
-//         .then((resp) => console.log(resp.status))
-//         .catch(() => console.log("all bad"));
-// }
-
-// getData(url)
-
-// setTimeout(() => {
-//     console.log(getData(url))
-// }, 5000);
 
 
 
