09f44b8222b30a225244e10414f7fbf02338a284
gramsco <antoningauth@gmail.com>
2019-08-31 18:13:55 +0200

    Structure done

--- app.js
+++ app.js
@@ -6,22 +6,60 @@ const path = require("path")
 const url = 'http://poetrydb.org/author/keats';
 const PORT = process.env.PORT || 7000;
 
+let bigdata = []
+
+axios.get(url).then(res => {
+
+    getData(res.data);
+}).catch(err => {
+    console.log(err)
+})
+
+function getData(data) {
+    data.forEach(element => {
+        bigdata.push(element)
+    });
+}
+
+setTimeout(() => {
+    console.log(`${bigdata.length} poems loaded`)
+}, 2500);
+
 
 app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "hbs")
 
-hbs.registerPartials(path.join(__dirname,"views/partials"))
+hbs.registerPartials(path.join(__dirname + "/views/partials"));
 
 app.get("/", (req, res) => {
     // res.send("all good");
-    axios.get(url)
-        .then((resp) => {
-            let random = Math.floor(Math.random()*resp.data.length)
-            let poem = resp.data[random]
-            res.render("index", poem)
-        })
-    .catch((err) => console.log(`oulah : ${err}`))
+
+    res.render("index", {})
+    
+})
+
+app.get("/", (req, res) => {
+    // res.send("all good");
+    res.render("index", {})
+})
+
+app.get("/poems", (req, res) => {
+    res.render("poems/poems", {poem:bigdata})
+})
+
+app.get("/poems/random", (req, res) => {
+    let random = Math.floor(Math.random() * bigdata.length)
+    let poem = bigdata[random]
+    res.render("poems/random", poem)
+})
+
+app.get("/poems/:id", (req, res) => {
+    res.render("poems/random", bigdata[req.params.id])
+})
+
+app.get("/:anything", (req, res) => {
+   res.render("wrong")
 })
 
 app.listen(PORT, () => console.log(`works at http://localhost:${PORT}`));
